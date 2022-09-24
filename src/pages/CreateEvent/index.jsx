3157638d778e470a2ece68581c47d3c0af58b532
EseAlli <essealli.ea@gmail.com>
2022-09-24 01:51:37 +0100

    create event page

--- src/pages/CreateEvent/index.jsx
+++ src/pages/CreateEvent/index.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <div className="create-event">
+      <div className="event-form-header">
+        <h1>Create Event</h1>
+      </div>
+      <form>
+        <div>
+          <label htmlFor="name">Event Name</label>
+          <input name="name" />
+        </div>
+        <div>
+          <label htmlFor="name">Host Name</label>
+          <input name="name" />
+        </div>
+        <div>
+          <label htmlFor="name">Location</label>
+          <input name="name" />
+        </div>
+        <div>
+          <label htmlFor="name">Start Date</label>
+          <input type="datetime-local" name="name" />
+        </div>
+        <div>
+          <label htmlFor="name">End Date</label>
+          <input type="datetime-local" name="name" />
+        </div>
+        <div>
+          <input name="name" type="file" />
+        </div>
+        <div className="cta">
+          <button>Create Event</button>
+          <Link to="/event">Next</Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default index;
